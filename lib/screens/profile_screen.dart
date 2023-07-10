bc21b9df70d5c770188b1190fb785cad0f0b26bc
Vlad Nechaev <nechaevvlad06@gmail.com>
2023-07-10 20:38:29 +0300

    profile screen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -4,16 +4,49 @@ import 'package:flutter/material.dart';
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
+  Widget buildButton(
+      String title, String subTitle, IconData icon, Function onPressed) {
+    return SizedBox(
+      width: 350,
+      child: OutlinedButton(
+        onPressed: onPressed as void Function()?,
+        child: ListTile(
+          leading: Icon(icon),
+          title: Text(title),
+          subtitle: Text(subTitle),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(
-        children: [
-          OutlinedButton(
-            onPressed: () => FirebaseAuth.instance.signOut(),
-            child: const Text("Sign Out"),
-          ),
-        ],
+      child: Padding(
+        padding: const EdgeInsets.only(top: 40, bottom: 40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            buildButton(
+                "Profile", "your information", Icons.person_rounded, () {}),
+            buildButton("My Orders", "keep track of current orders",
+                Icons.list_alt_rounded, () {}),
+            buildButton(
+                "Stores Location",
+                "find out where the nearest stores are",
+                Icons.location_on_rounded,
+                () {}),
+            buildButton(
+                "Delivery And Payment",
+                "find out all payment and delivery options",
+                Icons.payment_rounded,
+                () {}),
+            buildButton(
+                "Feedback", "ask us a question", Icons.feedback_rounded, () {}),
+            buildButton("Sign Out", "sign out of your account",
+                Icons.logout_rounded, () => FirebaseAuth.instance.signOut()),
+          ],
+        ),
       ),
     );
   }
