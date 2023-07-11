f165fb0d5bd47cecb840e970420615ca30f181e7
Vlad Nechaev <nechaevvlad06@gmail.com>
2023-07-11 14:11:57 +0300

    profile screen bug fixes

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -4,21 +4,6 @@ import 'package:flutter/material.dart';
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
-  Widget buildButton(
-      String title, String subTitle, IconData icon, Function onPressed) {
-    return SizedBox(
-      width: 350,
-      child: OutlinedButton(
-        onPressed: () => onPressed,
-        child: ListTile(
-          leading: Icon(icon),
-          title: Text(title),
-          subtitle: Text(subTitle),
-        ),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -28,24 +13,63 @@ class ProfileScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             buildButton(
-                "Profile", "your information", Icons.person_rounded, () {}),
-            buildButton("My Orders", "keep track of current orders",
-                Icons.list_alt_rounded, () {}),
+              "Profile",
+              "your information",
+              Icons.person_rounded,
+              () {},
+            ),
             buildButton(
-                "Stores Location",
-                "find out where the nearest stores are",
-                Icons.location_on_rounded,
-                () {}),
+              "My Orders",
+              "keep track of current orders",
+              Icons.list_alt_rounded,
+              () {},
+            ),
             buildButton(
-                "Delivery And Payment",
-                "find out all payment and delivery options",
-                Icons.payment_rounded,
-                () {}),
+              "Stores Location",
+              "find out where the nearest stores are",
+              Icons.location_on_rounded,
+              () {},
+            ),
             buildButton(
-                "Feedback", "ask us a question", Icons.feedback_rounded, () {}),
-            buildButton("Sign Out", "sign out of your account",
-                Icons.logout_rounded, () => FirebaseAuth.instance.signOut()),
+              "Delivery And Payment",
+              "find out all payment and delivery options",
+              Icons.payment_rounded,
+              () {},
+            ),
+            buildButton(
+              "Feedback",
+              "ask us a question",
+              Icons.feedback_rounded,
+              () {},
+            ),
+            buildButton(
+              "Sign Out",
+              "sign out of your account",
+              Icons.logout_rounded,
+              () {
+                FirebaseAuth.instance.signOut();
+              },
+            ),
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildButton(
+    String title,
+    String subTitle,
+    IconData icon,
+    Function onPressed,
+  ) {
+    return SizedBox(
+      width: 350,
+      child: OutlinedButton(
+        onPressed: () => onPressed(),
+        child: ListTile(
+          leading: Icon(icon),
+          title: Text(title),
+          subtitle: Text(subTitle),
         ),
       ),
     );
