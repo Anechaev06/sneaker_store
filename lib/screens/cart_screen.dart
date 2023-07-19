6ad0ffe2f0bdd9f87b8ac721eff5a4410b8e1543
Vlad Nechaev <nechaevvlad06@gmail.com>
2023-07-19 22:54:55 +0300

    refactoring navigation_screen

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import '../services/favorites_service.dart';
-import '../widgets/sneaker_tile.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(padding: const EdgeInsets.all(15.0), child: Column());
+    return const Padding(padding: EdgeInsets.all(15.0), child: Column());
   }
 }
--- lib/screens/navigation_screen.dart
+++ lib/screens/navigation_screen.dart
@@ -21,83 +21,86 @@ class NavigationScreen extends StatefulWidget {
 class _NavigationScreenState extends State<NavigationScreen> {
   int _selectedIndex = 0;
   final AuthService _authService = AuthService();
+  final PageController _pageController = PageController();
   bool _isAdmin = false;
-
-  void _onTap(int index) => setState(() => _selectedIndex = index);
-
-  Future<void> checkAdmin(User? user) async {
-    if (user != null) {
-      DocumentSnapshot userDoc = await FirebaseFirestore.instance
-          .collection('users')
-          .doc(user.uid)
-          .get();
-      _isAdmin = userDoc.get('isAdmin') ?? false;
-    } else {
-      _isAdmin = false;
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<User?>(
-        stream: _authService.authStateChanges,
-        builder: (BuildContext context, AsyncSnapshot<User?> snapshot) {
-          User? user = snapshot.data;
+      stream: _authService.authStateChanges,
+      builder: (BuildContext context, AsyncSnapshot<User?> snapshot) {
+        User? user = snapshot.data;
 
-          checkAdmin(user);
+        if (user != null) {
+          FirebaseFirestore.instance
+              .collection('users')
+              .doc(user.uid)
+              .get()
+              .then((userDoc) {
+            setState(() {
+              _isAdmin = userDoc.get('isAdmin') ?? false;
+            });
+          });
+        } else {
+          _isAdmin = false;
+        }
 
-          List<Widget> screens = [
-            const HomeScreen(),
-            const FavoritesScreen(),
-            _isAdmin
-                ? const AdminScreen()
-                : const CartScreen(), // Only for admins
-            user == null ? const AuthScreen() : const ProfileScreen(),
-          ];
-
-          return SafeArea(
-            child: Scaffold(
-              body: IndexedStack(
-                index: _selectedIndex,
-                children: screens,
-              ),
-              bottomNavigationBar: Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 10, vertical: 25),
-                child: ClipRRect(
-                  borderRadius: const BorderRadius.all(Radius.circular(50)),
-                  child: GNav(
-                    tabBorderRadius: 50,
-                    onTabChange: (index) => _onTap(index),
-                    tabActiveBorder: Border.all(color: Colors.black, width: 2),
-                    selectedIndex: _selectedIndex,
-                    tabBorder: Border.all(color: Colors.black, width: 2),
-                    backgroundColor: Colors.black,
-                    color: Colors.white,
-                    activeColor: secondColor,
-                    tabBackgroundColor: primaryColor,
-                    duration: const Duration(milliseconds: 150),
-                    gap: 5,
-                    tabs: [
-                      const GButton(
-                        icon: Icons.home_rounded,
-                        text: "Home",
-                      ),
-                      const GButton(
-                          icon: Icons.favorite_rounded, text: "Likes"),
-                      GButton(
-                          icon: _isAdmin
-                              ? Icons.admin_panel_settings
-                              : Icons.shopping_cart_rounded,
-                          text: _isAdmin ? "Admin" : "Cart"),
-                      const GButton(
-                          icon: Icons.person_rounded, text: "Profile"),
-                    ],
-                  ),
+        return SafeArea(
+          child: Scaffold(
+            body: PageView(
+              controller: _pageController,
+              onPageChanged: (index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+              children: [
+                const HomeScreen(),
+                const FavoritesScreen(),
+                _isAdmin ? const AdminScreen() : const CartScreen(),
+                user == null ? const AuthScreen() : const ProfileScreen(),
+              ],
+            ),
+            bottomNavigationBar: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 25),
+              child: ClipRRect(
+                borderRadius: const BorderRadius.all(Radius.circular(50)),
+                child: GNav(
+                  tabBorderRadius: 50,
+                  onTabChange: (index) {
+                    setState(() {
+                      _selectedIndex = index;
+                    });
+                    _pageController.jumpToPage(index);
+                  },
+                  tabActiveBorder: Border.all(color: Colors.black, width: 2),
+                  selectedIndex: _selectedIndex,
+                  tabBorder: Border.all(color: Colors.black, width: 2),
+                  backgroundColor: Colors.black,
+                  color: Colors.white,
+                  activeColor: secondColor,
+                  tabBackgroundColor: primaryColor,
+                  duration: const Duration(milliseconds: 150),
+                  gap: 5,
+                  tabs: [
+                    const GButton(
+                      icon: Icons.home_rounded,
+                      text: "Home",
+                    ),
+                    const GButton(icon: Icons.favorite_rounded, text: "Likes"),
+                    GButton(
+                        icon: _isAdmin
+                            ? Icons.admin_panel_settings
+                            : Icons.shopping_cart_rounded,
+                        text: _isAdmin ? "Admin" : "Cart"),
+                    const GButton(icon: Icons.person_rounded, text: "Profile"),
+                  ],
                 ),
               ),
             ),
-          );
-        });
+          ),
+        );
+      },
+    );
   }
 }
