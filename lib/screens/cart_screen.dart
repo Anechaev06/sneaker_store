b2eeae06f1c19bbf5c3ed4c25fd3d78c62e0761d
Vlad Nechaev <nechaevvlad06@gmail.com>
2023-07-19 22:46:44 +0300

    start creating cart screen

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../services/favorites_service.dart';
+import '../widgets/sneaker_tile.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(padding: const EdgeInsets.all(15.0), child: Column());
   }
 }
